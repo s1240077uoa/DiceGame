ece35a53d5088703183b209a8a7d7d8a8e6236a0
s1240077 <s1240077@std1dc25.u-aizu.ac.jp>
2018-07-02 18:05:48 +0900

    add greet

--- DiceGame.java
+++ DiceGame.java
@@ -1,11 +1,22 @@
 import java.util.Random;
+import java.io.*;
+
 class DiceGame{
     public class Number{
 	public static final int N = 2;
     }
     
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
+	InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);
 
+        System.out.print("What is your name?\n> ");
+
+        String str = br.readLine();
+
+        System.out.println("Hello, "+ str + "!");
+
+	
 	Random ran = new Random();
 	int sum = 0;
 	
